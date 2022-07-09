375bc6f3823e86fc9f9112195569f0ee22c1cc61
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2022-07-09 15:23:43 +0530

    version 7

--- script.js
+++ script.js
@@ -1,158 +1,158 @@
-// spread opraere
+spread opraere
 
-// const globalPlayar = ["vishakh","marks","jobs"]
-// const globalPlayar1 = ["june","may","april"]
+const globalPlayar = ["vishakh","marks","jobs"]
+const globalPlayar1 = ["june","may","april"]
 
-// const all = [...globalPlayar,...globalPlayar1,"work"]
+const all = [...globalPlayar,...globalPlayar1,"work"]
 
-// console.log(all);
+console.log(all);
 
-// const globalPlayar = ["vishakh","marks","jobs"];
+const globalPlayar = ["vishakh","marks","jobs"];
 
-// const [first,second,third] = globalPlayar;
+const [first,second,third] = globalPlayar;
 
-// console.log(third);
+console.log(third);
 
-// const globalPlayar = {
-//     id : 0,
-//     name : "vishakh",
-//     psition:"FW"
-// };
+const globalPlayar = {
+    id : 0,
+    name : "vishakh",
+    psition:"FW"
+};
 
-// const {psition,...rest} = globalPlayar;
+const {psition,...rest} = globalPlayar;
 
-// console.log(rest);
+console.log(rest);
 
-// const myfunc = value => value/2;
+const myfunc = value => value/2;
 
-// console.log(myfunc(9));
+console.log(myfunc(9));
 
-// const globalPlayar = [
-//     {
-//         id : 8,
-//         name : "vishakh",
-//         psition:"FW"
-//     },
-//     {
-//         id : 5,
-//         name : "Allu",
-//         psition:"CB"
-//     },
-//     {
-//         id : 17,
-//         name : "Midhun",
-//         psition:"CM"
-//     }
+const globalPlayar = [
+    {
+        id : 8,
+        name : "vishakh",
+        psition:"FW"
+    },
+    {
+        id : 5,
+        name : "Allu",
+        psition:"CB"
+    },
+    {
+        id : 17,
+        name : "Midhun",
+        psition:"CM"
+    }
 
-// ];
+];
 
-// for(let i =0;i<=globalPlayar.length;i++){
-//     console.log(globalPlayar[i].name);
-// };
+for(let i =0;i<=globalPlayar.length;i++){
+    console.log(globalPlayar[i].name);
+};
 
-// const globalPlayar = [
-//     {
-//         id : 8,
-//         name : "vishakh",
-//         psition:"SS"
-//     },
-//     {
-//         id : 5,
-//         name : "Allu",
-//         psition:"CB"
-//     },
-//     {
-//         id : 6,
-//         name : "Midhun",
-//         psition:"CM"
-//     },
-//     {
-//         id : 88,
-//         name : "ikkaka",
-//         psition:"SS"
-//     },
-//     {
-//         id : 52,
-//         name : "gowery",
-//         psition:"CB"
-//     },
-//     {
-//         id : 11,
-//         name : "ibra",
-//         psition:"CM"
-//     },
-//     {
-//         id : 18,
-//         name : "azife",
-//         psition:"SS"
-//     },
-//     {
-//         id : 15,
-//         name : "gabri",
-//         psition:"CB"
-//     },
-//     {
-//         id : 7,
-//         name : "loan",
-//         psition:"CM"
-//     }
+const globalPlayar = [
+    {
+        id : 8,
+        name : "vishakh",
+        psition:"SS"
+    },
+    {
+        id : 5,
+        name : "Allu",
+        psition:"CB"
+    },
+    {
+        id : 6,
+        name : "Midhun",
+        psition:"CM"
+    },
+    {
+        id : 88,
+        name : "ikkaka",
+        psition:"SS"
+    },
+    {
+        id : 52,
+        name : "gowery",
+        psition:"CB"
+    },
+    {
+        id : 11,
+        name : "ibra",
+        psition:"CM"
+    },
+    {
+        id : 18,
+        name : "azife",
+        psition:"SS"
+    },
+    {
+        id : 15,
+        name : "gabri",
+        psition:"CB"
+    },
+    {
+        id : 7,
+        name : "loan",
+        psition:"CM"
+    }
 
-// ];
+];
 
-// const newarry = globalPlayar.filter(value=>value.psition==="SS");
+const newarry = globalPlayar.filter(value=>value.psition==="SS");
 
-// console.log(newarry)
+console.log(newarry)
 
-// const numA = [18, 48, 600, 70, 30, 34];
+const numA = [18, 48, 600, 70, 30, 34];
 
-// const Atotal = numA.reduce((total, value) => total + value,0);
-// console.log(Atotal);
+const Atotal = numA.reduce((total, value) => total + value,0);
+console.log(Atotal);
 
-// const newobj = globalPlayar.find(value=>value.id === 1);
+const newobj = globalPlayar.find(value=>value.id === 1);
 
-// const numA=["vishakh","allu","gabri","midhun","hari"];
+const numA=["vishakh","allu","gabri","midhun","hari"];
 
-// const sor = numA.sort();
+const sor = numA.sort();
 
-// const numA=[18,48,[600,[70],70,30],34,100,[700,[80],57],34,26,89,];
-// const hey = numA.flat(Infinity);
+const numA=[18,48,[600,[70],70,30],34,100,[700,[80],57],34,26,89,];
+const hey = numA.flat(Infinity);
 
-// const globalPlayar = {
-//   id: 8,
-//   name: "vishakh",
-//   age: 18,
-//   company: {
-//     company_nam: "apple",
-//   },
-// };
+const globalPlayar = {
+  id: 8,
+  name: "vishakh",
+  age: 18,
+  company: {
+    company_nam: "apple",
+  },
+};
 
-// if (globalPlayar?.company?.company_nam) {
-//   console.log(globalPlayar.company.company_nam);
-// }
+if (globalPlayar?.company?.company_nam) {
+  console.log(globalPlayar.company.company_nam);
+}
 
-// const pobj = {};
+const pobj = {};
 
-// pobj.pid = 21100456;
-// pobj.pnam = "ipod";
-// // pobj.getData = () => console.log(pobj.pid, pobj.pnam);
+pobj.pid = 21100456;
+pobj.pnam = "ipod";
+// pobj.getData = () => console.log(pobj.pid, pobj.pnam);
 
-// console.log(pobj);
+console.log(pobj);
 
-//temrature Fharan to Celciues
+temrature Fharan to Celciues
 
-// let temInF = 50;
+let temInF = 50;
 
-// let C = ((temInF - 32) * 5) / 9;
+let C = ((temInF - 32) * 5) / 9;
 
-// console.log(C);
+console.log(C);
 
-// Boolean
+Boolean
 
-// let Amark = 6;
+let Amark = 6;
 
-// let grade = Amark == 10;
+let grade = Amark == 10;
 
-// console.log(grade);
+console.log(grade);
 
 const globalPlayar = ["vishakh", "marks", "jobs"];
 
--- tutorial1.js
+++ tutorial1.js
@@ -1,44 +1,44 @@
-// function Red() {
-//   document.getElementById("box").style.backgroundColor = "red";
-// }
-// function Blue() {
-//   document.getElementById("box").style.backgroundColor = "blue";
-// }
-// function Green() {
-//   document.getElementById("box").style.backgroundColor = "green";
-// }
-// let n = 2;
-// function Squr(num) {
-//   let ans = num * num;
-//   return ans;
-// }
-// let sq1 = Squr(n);
-// let sq2 = Squr(4);
+function Red() {
+  document.getElementById("box").style.backgroundColor = "red";
+}
+function Blue() {
+  document.getElementById("box").style.backgroundColor = "blue";
+}
+function Green() {
+  document.getElementById("box").style.backgroundColor = "green";
+}
+let n = 2;
+function Squr(num) {
+  let ans = num * num;
+  return ans;
+}
+let sq1 = Squr(n);
+let sq2 = Squr(4);
 
-// console.log(sq1);
-// console.log(sq2);
+console.log(sq1);
+console.log(sq2);
 
-// setInterval(function () {
-//   console.log("testing_...");
-// }, 1000);
-// clearInterval(5000);
+setInterval(function () {
+  console.log("testing_...");
+}, 1000);
+clearInterval(5000);
 
-// const timer = setInterval(myTime, 1000);
+const timer = setInterval(myTime, 1000);
 
-// function myTime() {
-//   const dt = new Date();
-//   var elm = document.getElementById("demo");
+function myTime() {
+  const dt = new Date();
+  var elm = document.getElementById("demo");
 
-//   elm.innerHTML = Math.random() * 10;
-// }
+  elm.innerHTML = Math.random() * 10;
+}
 
-// function stopTime() {
-//   clearInterval(timer);
-// }
+function stopTime() {
+  clearInterval(timer);
+}
 
-// function startime() {
-//   setInterval(myTime, 1000);
-// }
+function startime() {
+  setInterval(myTime, 1000);
+}
 
 var complex = ["a" + "b"];
 
