3020bc6754b29b5bb564d55bbfddc30cfb236b3e
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2022-08-26 10:35:30 +0530

    new script

---
+++ node-core/script.js
@@ -0,0 +1,24 @@
+// console.log("Running.........");
+
+// function newFunction() {
+//   console.log("hello......");
+// }
+
+// newFunction();
+
+// const array = ["a", "b", "h"];
+// const newArray = [...array, "g"];
+
+// console.log(array);
+// console.log(newArray);
+
+// function newFunction() {
+//   return 19;
+// }
+
+// newFunction();
+// console.log(newFunction());
+
+newFunction = () => 109;
+
+console.log(newFunction());
